10c1bbd7aef607f658137b04599bfdc3adb677b7
AlexVechtomov <avechtomov.dev@ya.ru>
2020-05-24 22:59:44 +0400

    social linc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavigateComponent
+    NavigateComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
